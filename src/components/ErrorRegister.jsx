c4f842fdb78e556643e1a7d63bfac8e5e777c3de
Robert.minia <omid-bakeri>
2023-09-14 17:12:35 +0330

    file updated -- register

--- src/components/ErrorRegister.jsx
+++ src/components/ErrorRegister.jsx
@@ -1,11 +1,13 @@
-export default function ErrorRegister() {
+function ErrorRegister({ check }) {
   return (
     <div
       className="p-3 w-[94%] mx-auto selection:bg-slate-600 
-      rounded-sm bg-red-500 text-red-100 mb-2 hidden
+      rounded-sm bg-red-500 text-red-100 mb-2 
                 text-sm"
     >
-      کاربر گرامی لطفا تمام فیلد ها را پر نمایید.
+      {check}
     </div>
   );
 }
+
+export default ErrorRegister;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,7 +3,36 @@ import { FaEye } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import ErrorRegister from "../components/ErrorRegister";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const register = () => {
+  const [checkError, setCheckError] = useState(false);
+  const [checkErrorMessage, setCheckErrorMessage] = useState("");
+
+  const [fullName, setFullName] = useState();
+  const [username, setUsername] = useState();
+  const [date, setDate] = useState();
+  const [email, setEmail] = useState();
+  const [pass, setPass] = useState();
+  const [rePass, setRePass] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(fullName, username, date, email, pass, rePass);
+
+    if (!fullName || !username || !date || !email || !pass || !rePass) {
+      setCheckError(true);
+      setCheckErrorMessage("کاربر گرامی لطفا تمام فیلد ها را پر نمایید.");
+      return;
+    }
+
+    if (pass !== rePass) {
+      console.log("mokhalef");
+      setCheckError(true);
+      setCheckErrorMessage("کاربر گرامی رمز عبور و تکرار آن باید مشابه باشد.");
+      return;
+    }
+  }
   const buttonStyle =
     "bg-green-600 w-full w-[94%] mx-auto px-4 text-green-100 hover:opacity-90 py-3 mt-2 rounded-sm";
   return (
@@ -11,7 +40,13 @@ const register = () => {
       <div className="bg-gradient-to-r flex justify-center items-center h-screen from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...">
         <div className="h-[800px] p-4 rounded-lg shadow-sm opacity-90 w-[1200px] mx-auto">
           <div className="flex  justify-center items-center gap-3">
-            <form className="bg-slate-800 text-xl text-white rounded-lg shadow-lg h-[800px] p-4 w-[600px]">
+            <form
+              onSubmit={handleSubmit}
+              method="post"
+              className="bg-slate-800 text-xl
+             text-white rounded-lg shadow-lg
+              h-[800px] p-4 w-[600px]"
+            >
               <div
                 className="flex
                justify-center rounded-sm
@@ -30,13 +65,16 @@ const register = () => {
                  h-[89.3%]
                rounded-lg shadow-lg p-4 bg-slate-700"
               >
-                <ErrorRegister />
+                {checkError && <ErrorRegister check={checkErrorMessage} />}
                 <div
                   className="flex p-4  mb-1
                  font-regular justify-between items-center gap-3"
                 >
                   <label className="text-lg">نام و نام خانوادگی</label>
                   <input
+                    onFocus={() => setCheckError(false)}
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
                     type="text"
                     className="w-[335px] outline-none
                      rounded-md px-4 text-slate-800
@@ -52,6 +90,9 @@ const register = () => {
                 >
                   <label className="text-lg">نام کاربری</label>
                   <input
+                    onFocus={() => setCheckError(false)}
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                     type="text"
                     className="w-[335px] outline-none
                      rounded-md px-4 text-slate-800
@@ -67,6 +108,9 @@ const register = () => {
                 >
                   <label className="text-lg">تاریخ تولد</label>
                   <input
+                    onFocus={() => setCheckError(false)}
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
                     type="date"
                     className="w-[335px] outline-none
                      rounded-md px-4 text-slate-800
@@ -82,6 +126,9 @@ const register = () => {
                 >
                   <label className="text-lg">آدرس الکترونیکی</label>
                   <input
+                    onFocus={() => setCheckError(false)}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     type="email"
                     className="w-[335px] outline-none
                      rounded-md px-4 text-slate-800
@@ -98,6 +145,9 @@ const register = () => {
                   <label className="text-lg">رمز عبور</label>
                   <div className="flex relative  justify-end items-center gap-2 ">
                     <input
+                      onFocus={() => setCheckError(false)}
+                      value={pass}
+                      onChange={(e) => setPass(e.target.value)}
                       type="password"
                       className=" outline-none
                      rounded-md px-4 text-slate-800
@@ -121,6 +171,9 @@ const register = () => {
                   <label className="text-lg">تکرار رمز عبور</label>
                   <div className="flex relative  justify-end items-center gap-2 ">
                     <input
+                      onFocus={() => setCheckError(false)}
+                      value={rePass}
+                      onChange={(e) => setRePass(e.target.value)}
                       type="password"
                       className=" outline-none
                      rounded-md px-4 text-slate-800
@@ -132,9 +185,10 @@ const register = () => {
                   </div>
                 </div>
                 <button
+                  type="submit"
                   className={`flex justify-center mt-5 ${buttonStyle} items-center gap-2`}
                 >
-                  <button>ثبت نام</button>
+                  <p>ثبت نام</p>
                   <i>
                     <FaUser />
                   </i>
